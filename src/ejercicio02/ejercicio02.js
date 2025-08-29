624a9242c0b8078b6cc572b45b582f59039aa7f0
Tolaba Brian <ezzeq.99@gmail.com>
2025-08-29 19:24:17 -0300

    Subi ejercicio 1 y 2

---
+++ src/ejercicio02/ejercicio02.js
@@ -0,0 +1,14 @@
+/*2.- Solicitar al usuario que ingrese tres números enteros, luego mostrar 
+como resultado el promedio de los tres. El promedio de tres números se 
+calcula sumando los tres números y dividiendo entre 3.
+*/
+
+alert("Buenas, acontinuacion ingresa tres numeros (enteros) para que podamos promediarlos");
+
+let numero1 = Number(prompt("Ingrese su PRIMER número"));
+let numero2 = Number(prompt("Ingrese su SEGUNDO número"));
+let numero3 = Number(prompt("Ingrese su TERCER número"));
+
+alert("El promedio es de " + ((numero1+numero2+numero3)/3));
+
+//falta el factor humano... prever cosas
